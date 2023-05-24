d38fc96770bf6a0e476c9b4aac89cd3cdc7d2c69
baggio-snir <Quentin.Chassel@ac-lille.fr>
2023-05-24 12:05:52 +0200

    Ajout base JS

--- media/scripts/main.js
+++ media/scripts/main.js
@@ -1,2 +1,33 @@
 
+const api = {
+    uri: {},
+    call: async function(uri, method = 'GET', data = {}){
+        const r = await fetch(uri, {
+            method: method,
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        return r.json();
+    }
+};
 
+const loadCategories = function(){};
+const loadCategory = function(){};
+
+const displayCategories = function(){};
+const displayCategory = function(){};
+
+const manualCall = function(){};
+const loadTableNum = function(){};
+const changeTableNum = function(){};
+
+const addToCart = function(){};
+const clearCart = function(){};
+
+document.addEventListener('load', () => {
+
+});
