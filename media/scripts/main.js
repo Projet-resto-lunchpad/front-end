a572369b7b72f8b8fff296eb1e3fd493709ac13c
baggio-snir <quentin.chassel@ac-lille.fr>
2023-05-25 08:56:33 +0200

    Add caveast for JS manip of templates

--- media/scripts/main.js
+++ media/scripts/main.js
@@ -1,6 +1,8 @@
 
 const api = {
-    uri: {},
+    uri: {
+        'categs': '/categories'
+    },
     call: async function(uri, method = 'GET', data = {}){
         const r = await fetch(uri, {
             method: method,
@@ -15,17 +17,47 @@ const api = {
     }
 };
 
-const loadCategories = function(){};
+const tpl = {
+    get: function(shortid){
+        const t = document.querySelector('#tpl_'+shortid);
+        return t? t.content.cloneNode(true):null;
+    }
+};
+
+const changeTitle = function(){};
+const clearContent = function(){
+    changeTitle('');
+    document.querySelector('#caveat').innerHTML = '';
+};
+
+const loadCategories = function(){
+    clearContent();
+    changeTitle('Chargement...');
+    api.call(api.categs).then((categories) => {
+        for(const categ in categories) {
+            const t = tpl.get('category');
+            if(t) {
+                formatCategory(t, categ);
+            }
+        }
+    });
+};
 const loadCategory = function(){};
 
-const displayCategories = function(){};
-const displayCategory = function(){};
+const formatCategory = function(template, category){
+
+};
+
+const formatProduct = function(template, product) {
+    
+};
 
 const manualCall = function(){};
 const loadTableNum = function(){};
 const changeTableNum = function(){};
 
 const addToCart = function(){};
+const changeCart = function(){};
 const clearCart = function(){};
 
 document.addEventListener('load', () => {
