5c733f6040387d61339495cf68972fa0b1a3d2da
SacepeFabien <sacepefabien@gmail.com>
2023-06-09 11:23:25 +0200

    Update index.php

--- api/table/index.php
+++ api/table/index.php
@@ -3,6 +3,28 @@ require_once('../_load.php');
 
 $r = [];
 
+$ip = $_SERVER['REMOTE_ADDR'];
+
+if($_m == 'GET'){
+$stmt = $_sql->prepare('select id from `tables` where `ip`=' $ip);
+$stmt->execute();
+$id = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$r[] = [
+    'table' => $id,
+    ];
+}
+elseif($_m == 'POST'){
+    
+    if($pwd == 'admin'){
+        $stmt = $_sql->prepare('insert into tables values ('$id ','$ip')');
+        $stmt->execute();
+    }
+
+    $r[] = [
+        'authorized' => false,
+        ];
+}
+
 echo json_encode($r);
 
 exit;
