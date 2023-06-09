94097626206de96b4921e7246d2aa187550c7da3
SacepeFabien <sacepefabien@gmail.com>
2023-06-09 12:25:25 +0200

    Update index.php

--- api/table/index.php
+++ api/table/index.php
@@ -5,25 +5,24 @@ $r = [];
 
 $ip = $_SERVER['REMOTE_ADDR'];
 
-if($_m == 'GET'){
-$stmt = $_sql->prepare('select id from `tables` where `ip`=' $ip);
-$stmt->execute();
-$id = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$r[] = [
-    'table' => $id,
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $stmt = $_sql->prepare("SELECT id FROM `tables` WHERE `ip` = ?");
+    $stmt->execute([$ip]);
+    $id = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $r[] = [
+        'table' => $id,
     ];
-}
-elseif($_m == 'POST'){
-    $pwd = isset($_POST["pwd"]);
-    $id = isset($_POST["t"]);
-    if($pwd == 'admin'){
-        $stmt = $_sql->prepare('insert into tables values ('$id ','$ip')');
-        $stmt->execute();
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $pwd = isset($_POST["pwd"]) ? $_POST["pwd"] : "";
+    $id = isset($_POST["t"]) ? $_POST["t"] : "";
+    if ($pwd == 'admin') {
+        $stmt = $_sql->prepare("INSERT INTO tables VALUES (?, ?)");
+        $stmt->execute([$id, $ip]);
     }
 
     $r[] = [
         'authorized' => false,
-        ];
+    ];
 }
 
 echo json_encode($r);
